668c4c726ae2fa6366d550f98f025cf3e317c4be
Bibhu <bibhu8917215985@gmail.com>
2026-05-26 23:51:42 +0530

    created DTO class

---
+++ src/main/java/com/bibhu/employeemanagementsystem/dto/EmployeeDTO.java
@@ -0,0 +1,61 @@
+package com.bibhu.employeemanagementsystem.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public class EmployeeDTO {
+
+    private Long id;
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Department is required")
+    private String department;
+
+    public EmployeeDTO() {
+    }
+
+    public EmployeeDTO(Long id, String name, String email, String department) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.department = department;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+}
